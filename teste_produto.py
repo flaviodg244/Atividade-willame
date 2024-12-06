09fd04e8e0d250fef9774bf7890533d141af74e2
flaviodg244 <149584387+flaviodg244@users.noreply.github.com>
2024-12-06 11:12:05 -0300

    Add files via upload

---
+++ teste_produto.py
@@ -0,0 +1,6 @@
+from produto import Produto
+produto1 = Produto("Camisa", "roupa", 50.0, 10)
+produto2 = Produto("Calça", "Roupa", 70.0, 5)
+
+produto1.incrementar_estoque(5)
+produto2.incrementar_estoque(3)
